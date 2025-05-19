81e061abf2a1734e3be1ed13036ffd8ebbd1b22d
savi-creates <savannahandresson@gmail.com>
2025-05-19 17:21:08 +0100

    Made some changes to make the structure better

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./styles.css";
+import Search from "./Search";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
+      <Search />
+
+      <footer style={{ marginTop: "2rem", textAlign: "center" }}>
+        {" "}
+        Coded by Savannah Andresson and {""}
+        <a
+          href="https://github.com/savi-creates/weather-app-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          is open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
