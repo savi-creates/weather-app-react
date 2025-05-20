647aa5604afb701a76f30b4942249f6449b70aad
savi-creates <savannahandresson@gmail.com>
2025-05-20 10:35:19 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,27 +4,29 @@ import Search from "./Search";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <Search />
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+        <Search />
 
-      <footer style={{ marginTop: "2rem", textAlign: "center" }}>
-        Coded by {""}
-        <a
-          href="https://github.com/savi-creates"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Savannah Andresson
-        </a>{" "}
-        and {""}
-        <a
-          href="https://github.com/savi-creates/weather-app-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          is open-sourced on GitHub
-        </a>
-      </footer>
+        <footer style={{ marginTop: "2rem", textAlign: "center" }}>
+          Coded by {""}
+          <a
+            href="https://github.com/savi-creates"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Savannah Andresson
+          </a>{" "}
+          and {""}
+          <a
+            href="https://github.com/savi-creates/weather-app-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            is open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
